0ddd04239d3a8e46950c8aff8925f720824577f2
Arindam Dey <73965355+Arindam515@users.noreply.github.com>
2021-06-09 23:05:13 +0600

    Update File Registration.php

--- Task1/File Registration.php
+++ Task1/File Registration.php
@@ -1,36 +1,69 @@
 <!DOCTYPE html>
 <html>
+<head>
+
+<title>Registration Form </title>
+
+</head>
 <body>
+<table>
+<h1>Registration Form</h1>
+<form>
+<tr>
+<td> <label for="Full_Name ">Full Name : </label> </td>
+<td><input type="text" id="Full_Name"></td>
+</tr>
 
-<h2>Registration Form </h2>
 
-<form action="/action_page.php">
-  <label for="fname">Full Name:</label><br>
-  <input type="text" id="fname" name="fname"><br><br>
-    <label1 for="fname">E-Mail:</label1><br>
-    <input type="text" id="fname" name="fname"><br><br>
-      <label2 for="fname">Password:</labe2><br>
-  <input type="text" id="fname" name="fname"><br><br>
-   <label3 for="fname">Comment  :</label3><br>
-  <input type="text" id="fname" name="fname"><br><br>
-    <label4 for="fname">Gender  :</label4><br>
-  
-   <input type="radio" id="male" name="gender" value="male">
-  <label for="male">Male</label><br>
+<tr>
+<td> <label for="Email ">Email : </label> </td>
+<td><input type="text" id="Email"></td>
+</tr>
+
+
+<tr>
+<td> <label for="Password ">Password : </label> </td>
+<td><input type="password" id="Password"></td>
+</tr>
+
+<tr>
+<td> <label for="Comment ">Comment : </label> </td>
+<td><textarea rows="5" cols="30" name="comment" form="Comment  Here "> </textarea></td>
+</tr>
+
+
+<tr>
+<td> <label for="Gender ">Gender : </label> </td>
+<td>
+<input type="radio" id="male" name="gender" value="male">
+<label for="male">Male</label>
   <input type="radio" id="female" name="gender" value="female">
-  <label for="female">Female</label><br>
+  <label for="female">Female</label>
   <input type="radio" id="other" name="gender" value="other">
-  <label for="other">Other </label>
-    <p>Show a file-select field which allows a file to be chosen for upload:</p>
-<form action="/action_page.php">
-  <label for="myfile">Select a file:</label>
-  <input type="file" id="myfile" name="myfile"><br><br>
-  <input type="submit" value="Submit">
-   <input type="Reset" value="Reset">
-  
-  
- 
+  <label for="other">Other</label>
+</td>
+</tr>
+<tr>
+<td> <label for="Checkbox ">Please Choose a Hobby : </label> </td>
+<td>
+<input type="checkbox"  name="value1" value="Singing">
+<label for="value1">Singing</label>
+<input type="checkbox" name="value2" value="Dancing">
+<label for="value2">Dancing</label>
+<input type="checkbox"  name="value3" value="Reading">
+<label for="value3"> Reading</label>
+</td>
+</tr>
+<tr>
+<td> <label for="file ">Please Choose a File : </label> </td>
+<td><input type="file" id="file"></td>
+</tr>
+<tr>
+<td> <input type="submit" name="Submit"> 
+<input type="reset" name="Reset">
+</td>
+</tr>
 </form>
-
+</table>
 </body>
 </html>
