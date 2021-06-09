62c63e094598ee99943c7384c66c4352d96f7d26
Firstname Lastname <73965355+Arindam515@users.noreply.github.com>
2021-06-09 16:39:04 +0600

    file added

---
+++ Task1/File Registration.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Registration Form </h2>
+
+<form action="/action_page.php">
+  <label for="fname">Full Name:</label><br>
+  <input type="text" id="fname" name="fname"><br><br>
+    <label1 for="fname">E-Mail:</label1><br>
+    <input type="text" id="fname" name="fname"><br><br>
+      <label2 for="fname">Password:</labe2><br>
+  <input type="text" id="fname" name="fname"><br><br>
+   <label3 for="fname">Comment  :</label3><br>
+  <input type="text" id="fname" name="fname"><br><br>
+    <label4 for="fname">Gender  :</label4><br>
+  
+   <input type="radio" id="male" name="gender" value="male">
+  <label for="male">Male</label><br>
+  <input type="radio" id="female" name="gender" value="female">
+  <label for="female">Female</label><br>
+  <input type="radio" id="other" name="gender" value="other">
+  <label for="other">Other </label>
+    <p>Show a file-select field which allows a file to be chosen for upload:</p>
+<form action="/action_page.php">
+  <label for="myfile">Select a file:</label>
+  <input type="file" id="myfile" name="myfile"><br><br>
+  <input type="submit" value="Submit">
+   <input type="Reset" value="Reset">
+  
+  
+ 
+</form>
+
+</body>
+</html>
